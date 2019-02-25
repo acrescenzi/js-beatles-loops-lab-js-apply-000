76d5bbca61e14421a92251a51959b90919fc0f9c
Adriana Crescenzi <adrianacrescenzi@yahoo.com>
2019-02-25 19:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function theBeatlesPlay(arrM, arrI){
-  var newArr = []
-  for(let i = 0; i < arrM.length; i++){
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return newArr;
+
+  return arr
 }
 
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
 
-// + Create a function `theBeatlesPlay`, which accepts two parameters- an array of
-// musicians and an array of instruments. The body of the function should create an
-// empty array stored in a variable. The function should also contain a for loop
-// which loops over the array of musicians. You'll want to be careful about what
-// value you set your counter variable to store. (Hint: Think about what the first
-// index of an array is). The first time through the loop, the body of the loop
-// should create a string using the first index of the musicians array and the
-// first index of the instruments array: `"John Lennon plays guitar"`. This string
-// should be added to the empty array you created. The loop should make the same
-// sentence for every member of the musicians array. The function should return the
-// array of new strings.
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
